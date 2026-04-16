6ade35c58fe4b3bf236a6b36025d83878e1b4097
anton-gasse <anton.gasse@gmx.de>
2026-04-16 16:59:48 +0200

    refactor: vision function

--- embodied/run/eval_only.py
+++ embodied/run/eval_only.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from functools import partial as bind
+import embodied.core.vision as vision
 
 import elements
 import embodied
@@ -48,11 +49,11 @@ def eval_only(make_agent, make_env, make_logger, args):
           'score': result.pop('score'),
           'length': result.pop('length'),
       }, prefix='episode')
-      
+
       rew = result.pop('rewards')
       if len(rew) > 1:
         result['reward_rate'] = (np.abs(rew[1:] - rew[:-1]) >= 0.01).mean()
-      
+
       if args["task"].startswith("cr-atari"):
         vision_square_size = args["cr-atari.vision_square_size"]
         size = args["cr-atari.size"]
@@ -60,25 +61,24 @@ def eval_only(make_agent, make_env, make_logger, args):
         scale_gaze_scanpath_image = 100
         gaze_scanpath_image = np.zeros((size[0]*scale_gaze_scanpath_image, size[1]*scale_gaze_scanpath_image, 1), dtype=np.uint8)
         gaze_positions = result.pop("gaze_positions")
-        vision_squares = (
-                    size[0] // vision_square_size[0],
-                    size[1] // vision_square_size[1],
-                )
+        vision_square_count = vision.calc_vision_square_count(size=size, vision_square_size=vision_square_size)
         last_x_scaled = None
         last_y_scaled = None
         for i, gaze_position in enumerate(gaze_positions):
-        
-          x = gaze_position % vision_squares[0] * vision_square_size[0] + np.round(np.random.rand() * (vision_square_size[0]-1)).astype('int32')
-          y = gaze_position // vision_squares[0] * vision_square_size[1] + np.round(np.random.rand() * (vision_square_size[1]-1)).astype('int32')
-                
+
+          x, y = vision.convert_1d_vision_square_position_to_2d_random(
+              vision_square_position=gaze_position,
+              vision_square_count=vision_square_count,
+              vision_square_size=vision_square_size
+          )
           gaze_heatmap_image[y,x] += 1
-          
+
           x_scaled = x * scale_gaze_scanpath_image
           y_scaled = y * scale_gaze_scanpath_image
-          
+
           if last_x_scaled is not None and last_y_scaled is not None:
               cv2.line(gaze_scanpath_image, (last_x_scaled, last_y_scaled), (x_scaled, y_scaled), (255), 1)
-          
+
           cv2.putText(
               gaze_scanpath_image,
               str(i+1),
@@ -91,13 +91,13 @@ def eval_only(make_agent, make_env, make_logger, args):
           )
           last_x_scaled = x_scaled
           last_y_scaled = y_scaled
-        
+
         # Normalize image from 0 to 1 -> scale from 0 to 255
         gaze_heatmap_image = ((gaze_heatmap_image - gaze_heatmap_image.min()) * (1/(gaze_heatmap_image.max() - gaze_heatmap_image.min()) * 255)).astype('uint8')
-        logger.image("gaze_heatmap", gaze_heatmap_image)      
+        logger.image("gaze_heatmap", gaze_heatmap_image)
         logger.image("gaze_scanpath", gaze_scanpath_image)
         logger.add({'gaze_positions': str(gaze_positions)})
-            
+
       epstats.add(result)
 
   fns = [bind(make_env, i) for i in range(args.envs)]
