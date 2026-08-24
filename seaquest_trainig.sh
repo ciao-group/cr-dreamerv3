0f81911e7adb0913574b154658e4b0e8be91cb7c
Martin Lorenz <25765799+martinoz1024@users.noreply.github.com>
2026-08-24 17:04:46 +0200

    feat: dynamic path generation with naming

--- seaquest_trainig.sh
+++ seaquest_trainig.sh
@@ -10,14 +10,24 @@
 # Define the game variable to be used throughout the script
 GAME="seaquest"
 
+# Extract the script filename without the extension to use as the log folder name
+SCRIPT_NAME=$(basename "$0")
+SCRIPT_NAME_NO_EXT="${SCRIPT_NAME%.*}"
+
 WORKSPACE_DIR=$(ws_find DreamerSpace)
+
+# Define the common log directory dynamically for both training and evaluation
+LOG_DIR="${WORKSPACE_DIR}/${GAME}/logdir/cr-dreamer/${GAME}/${SCRIPT_NAME_NO_EXT}"
 
 cd ~/cr-dreamer/cr-dreamerv3
 source dreamerv3/bin/activate
 
-python dreamerv3/main.py  --logdir ${WORKSPACE_DIR}/${GAME}/logdir/cr-dreamer/${GAME}/motor_delay --run.train_ratio 32 --configs cr-atari --task cr-atari_${GAME} --env.cr-atari.vision_square_size 24,24 --jax.policy_devices 0,1,2,3,4,5,6,7 --jax.train_devices 0,1,2,3,4,5,6,7 --env.cr-atari.pooling 2 --env.cr-atari.vision_model EMMA --env.cr-atari.vision_mode foveated --env.cr-atari.repeat 4 --env.cr-atari.motor_action_delay True --env.cr-atari.reaction_time_delay True --env.cr-atari.eye_movement_effort True
+# Training command
+# python dreamerv3/main.py --logdir ${LOG_DIR} --run.train_ratio 32 --configs cr-atari --task cr-atari_${GAME} --env.cr-atari.vision_square_size 24,24 --jax.policy_devices 0,1,2,3,4,5,6,7 --jax.train_devices 0,1,2,3,4,5,6,7 --env.cr-atari.pooling 2 --env.cr-atari.vision_model EMMA --env.cr-atari.vision_mode foveated --env.cr-atari.repeat 4 --env.cr-atari.motor_action_delay True --env.cr-atari.reaction_time_delay True --env.cr-atari.eye_movement_effort True
 
+# Dynamically find the most recently modified checkpoint directory
+LATEST_CKPT=$(ls -td ${LOG_DIR}/ckpt/*/ | head -n 1)
 
-# python dreamerv3/main.py  --logdir ${WORKSPACE_DIR}/${GAME}/logdir/cr-dreamer/${GAME}/emma_pooling_2_periphery-cutoff_eval --run.train_ratio 32 --configs cr-atari --task cr-atari_${GAME} --env.cr-atari.vision_square_size 24,24 --jax.policy_devices 0 --jax.train_devices 0 --env.cr-atari.pooling 2 --script eval_only --run.from_checkpoint /work2/wy39otun-CRDreamer/${GAME}/logdir/cr-dreamer/${GAME}/emma_pooling_2_periphery-cutoff/ckpt/20260620T001720F088682/ --env.cr-atari.vision_model EMMA --env.cr-atari.vision_mode periphery-cutoff --run.steps 5e5
-
+# Evaluation command
+python dreamerv3/main.py --logdir ${LOG_DIR} --run.train_ratio 32 --configs cr-atari --task cr-atari_${GAME} --env.cr-atari.vision_square_size 24,24 --jax.policy_devices 0 --jax.train_devices 0 --env.cr-atari.pooling 2 --script eval_only --run.from_checkpoint ${LATEST_CKPT} --env.cr-atari.vision_model EMMA --env.cr-atari.vision_mode periphery-cutoff --run.steps 5e5
 # python dreamerv3/main.py  --logdir ${WORKSPACE_DIR}/${GAME}/logdir/cr-dreamer/${GAME}/emma_eval_gaze_model --run.train_ratio 32 --configs cr-atari --task cr-atari_${GAME} --env.cr-atari.vision_square_size 24,24 --env.cr-atari.pooling 4 --script eval_gaze --run.from_checkpoint /work2/wy39otun-CRDreamer/${GAME}/logdir/cr-dreamer/${GAME}/emma/ckpt/20260610T181156F882891/ --env.cr-atari.vision_model EMMA --env.cr-atari.vision_mode foveated --evaluation.apply_vision_square_from model
